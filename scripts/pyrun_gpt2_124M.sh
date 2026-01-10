f4bab21d0cf083238ef61174edc1e03b4630f92d
insop <insop.song@gmail.com>
2026-01-10 03:52:36 +0000

    updated scrhipt

--- scripts/pyrun_gpt2_124M.sh
+++ scripts/pyrun_gpt2_124M.sh
@@ -5,11 +5,11 @@
 # if you wish to train on just a single GPU, simply skip the torchrun part, i.e.
 # python train_gpt2.py ... (all the other arguments the same)
 torchrun --standalone --nproc_per_node=8 train_gpt2.py \
-    --input_bin "dev/data/fineweb10B/fineweb_train_*.bin" \
-    --input_val_bin "dev/data/fineweb10B/fineweb_val_*.bin" \
+    --input_bin "/workspace/datasets/fineweb10B/fineweb_train_*.bin" \
+    --input_val_bin "/workspace/datasets/fineweb10B/fineweb_val_*.bin" \
     --val_loss_every 250 \
     --sample_every 0 \
-    --output_dir pylog_gpt2_124M \
+    --output /workspace/logs/ng/base \
     --write_tensors 0 \
     --model d12 \
     --batch_size 32 \
@@ -26,3 +26,4 @@ torchrun --standalone --nproc_per_node=8 train_gpt2.py \
     --warmup_iters 700 \
     --learning_rate_decay_frac 0.0 \
     --overfit_single_batch 0
+
